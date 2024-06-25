b1b51cddd7c431ba3a64d5c7c06942e81b8a9ad2
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-06-25 09:37:10 -0400

    part one complete

---
+++ myapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    FEEDBACK_CHOICES = [
+        ('B', 'Borrow'),
+        ('P', 'Purchase'),
+    ]
+    feedback = forms.ChoiceField(choices=FEEDBACK_CHOICES)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
+from myapp.forms import FeedbackForm
 from myapp.models import Book
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -12,6 +14,26 @@ def index(request):
 def about(request):
     return render(request, 'myapp/about.html')
 
+
 def detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     return render(request, 'myapp/detail.html', {'book': book})
+
+
+def getFeedback(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.cleaned_data['feedback']
+            if feedback == 'B':
+                choice = 'to borrow books.'
+            elif feedback == 'P':
+                choice = 'to purchase books.'
+            else:
+                choice = 'None.'
+            return render(request, 'myapp/fb_results.html', {'choice': choice})
+        else:
+            return HttpResponse('Invalid data')
+    else:
+        form = FeedbackForm()
+        return render(request, 'myapp/feedback.html', {'form': form})
